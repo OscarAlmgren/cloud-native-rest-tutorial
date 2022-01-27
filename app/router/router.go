96bce822bbbab31a0bd1e3fc7263812a40951188
Oscar Almgren <oscar.almgren@gmail.com>
2022-01-27 10:15:26 +0100

    lade till nya router paths med matchande handlers.

--- app/router/router.go
+++ app/router/router.go
@@ -14,5 +14,10 @@ func New(server *server.Server) *chi.Mux {
 	chiRouter.Method("GET", "/", requestlog.NewHandler(server.HandleIndex, logger))
 	chiRouter.Get("/healthz/readiness", server.HandleLive)
 
+	chiRouter.Method("GET", "/books", requestlog.NewHandler(server.HandleListBooks, logger))
+	chiRouter.Method("POST", "books", requestlog.NewHandler(server.HandleCreateBook, logger))
+	chiRouter.Method("GET", "/books/{id}", requestlog.NewHandler(server.HandleReadBook, logger))
+	chiRouter.Method("PUT", "/books/{id}", requestlog.NewHandler(server.HandleDeleteBook, logger))
+
 	return chiRouter
 }
