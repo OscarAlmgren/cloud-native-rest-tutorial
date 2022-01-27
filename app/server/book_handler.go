e53bfa519df60185af0cb5e056f589cc9d9558dc
Oscar Almgren <oscar.almgren@gmail.com>
2022-01-27 10:05:03 +0100

    Lade till CreateBook, ReadBook, UpdateBook, DeleteBook

--- app/server/book_handler.go
+++ app/server/book_handler.go
@@ -6,3 +6,20 @@ func (server *Server) HandleListBooks(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("[]"))
 }
+
+func (server *Server) HandleCreateBook(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (server *Server) HandleReadBook(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("{}"))
+}
+
+func (server *Server) HandleUpdateBook(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (server *Server) HandleDeleteBook(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusAccepted)
+}
